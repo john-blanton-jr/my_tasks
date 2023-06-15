957344048f88028c2a93ba7b427aad196608a8a2
John Blanton <johnbjr76@gmail.com>
2023-06-15 10:59:37 -0600

    create model for tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    task = models.CharField(max_length=200)
+    completed = models.models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
