7de110c4559ad5c71dd20a92ba91e06c8e302e36
John Blanton <johnbjr76@gmail.com>
2023-06-15 10:31:53 -0600

    set up first view and update app and project urls.py, test HttpResponse works

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def show_tasks(request):
+    return HttpResponse("This view is working")
