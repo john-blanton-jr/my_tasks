cfa552fea27e5ec97b9e577b1c943864a08a36a5
John Blanton <johnbjr76@gmail.com>
2023-06-15 11:36:42 -0600

    add model to admin migrate models

--- tasks/models.py
+++ tasks/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Task(models.Model):
     task = models.CharField(max_length=200)
-    completed = models.BooleanField(default=True)
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.name
+        return self.task
