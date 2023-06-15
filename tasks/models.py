34b75c6d8b3e6448047eead46b9222770e17a209
John Blanton <johnbjr76@gmail.com>
2023-06-15 11:01:37 -0600

    fix mispelling in model

--- tasks/models.py
+++ tasks/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Task(models.Model):
     task = models.CharField(max_length=200)
-    completed = models.models.BooleanField(default=True)
+    completed = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
