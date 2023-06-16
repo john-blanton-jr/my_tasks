039d344ba8b9b7d6f3384722910ac3e15561bc29
John Blanton <johnbjr76@gmail.com>
2023-06-16 05:46:55 -0600

    update task template to sidplay tasks. Create delete view and add to template.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from tasks.views import show_tasks
+from tasks.views import show_tasks, delete
 
 urlpatterns = [
-    path("", show_tasks),
+    path("", show_tasks, name="show_tasks"),
+    path("delete/<int:id>", delete, name="delete"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from tasks.models import Task
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 
 def show_tasks(request):
+    tasks = Task.objects.all()
+    context = {
+        "tasks": tasks
+    }
+    return render(request, "tasks/tasks.html", context)
 
-    return render(request, "tasks/tasks.html")
+
+def delete(request, id):
+    item = Task.objects.get(id=id)
+    item.delete()
+    return HttpResponseRedirect(reverse('show_tasks'))
