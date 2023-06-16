91ab53285385aeafbfa8f7f7db21235ed08fddcc
John Blanton <johnbjr76@gmail.com>
2023-06-16 06:39:50 -0600

    add form to add task in show_tasks view, add to html and test

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from tasks.views import show_tasks, delete
+from tasks.views import show_tasks, delete, mark_completed
 
 urlpatterns = [
     path("", show_tasks, name="show_tasks"),
     path("delete/<int:id>", delete, name="delete"),
+    path("mark_completed/<int:id>", mark_completed, name="mark_completed"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from tasks.models import Task
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
+from tasks.forms import TaskForm
 
 
 def show_tasks(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("show_tasks")
+    else:
+        form = TaskForm()
+    context = {
+        "form": form,
+    }
+
     tasks = Task.objects.all()
     context = {
-        "tasks": tasks
+        "tasks": tasks,
+        "form": form
     }
     return render(request, "tasks/tasks.html", context)
 
@@ -15,4 +28,11 @@ def show_tasks(request):
 def delete(request, id):
     item = Task.objects.get(id=id)
     item.delete()
+    return HttpResponseRedirect(reverse('show_tasks'))
+
+
+def mark_completed(request, id):
+    task = Task.objects.get(id=id)
+    task.completed = True
+    task.save()
     return HttpResponseRedirect(reverse('show_tasks'))
